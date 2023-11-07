9e7cc543660d0b9268bd41e3fdde2473b20e39d9
Vorona <oleksandr.vorona@sigma.software>
2023-11-07 15:58:45 +0200

    Removed unused code in program.cs

--- BlogMVC/Program.cs
+++ BlogMVC/Program.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using BlogMVC.Models;
 using Microsoft.AspNetCore.Identity;
 using BlogMVC.BLL;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("BlogMVCContext") ?? throw new InvalidOperationException("Connection string 'BlogMVCContext' not found.");
+var connectionString = builder.Configuration.GetConnectionString("BlogMVCContext") 
+    ?? throw new InvalidOperationException("Connection string 'BlogMVCContext' not found.");
 Startup.Configure(builder.Services, connectionString);
 
 // Add services to the container.
-//builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<BlogMVCContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<IdentityOptions>(options =>
@@ -39,7 +36,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
